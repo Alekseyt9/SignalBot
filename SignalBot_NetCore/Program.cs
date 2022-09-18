7ca68d5f74bc674db15b5b7013e39f617416051f
Alekseyt9 <alekseyt9@gmail.com>
2022-09-18 16:14:49 +0300

    delete unnecessary files

--- SignalBot_NetCore/Program.cs
+++ SignalBot_NetCore/Program.cs
@@ -3,15 +3,10 @@ using SignalBot_NetCore.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-/*
-builder.Services.AddHostedService<TelegramBackgroundService>();
-builder.Services.AddSingleton<MessagesStore>();
-*/
 
 builder.Services.AddSingleton<TelegramService>();
 
