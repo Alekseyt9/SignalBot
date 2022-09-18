5358050375db778e60beb90f896b9c7954f26c60
Alekseyt9 <alekseyt9@gmail.com>
2022-09-18 08:19:41 +0300

    First edition

---
+++ SignalBot_NetCore/Controllers/TelegramController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignalBot_NetCore.Services;
+
+namespace SignalBot_NetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelegramController : ControllerBase
+    {
+        TelegramService _svc;
+
+        public TelegramController(TelegramService svc)
+        {
+            _svc = svc;
+        }
+
+        [HttpGet(Name = "SendMessage")]
+        public async Task<string> GetAsync(string msg)
+        {
+            await _svc.SendMessageAsync(msg);
+            return $"Message sended: {msg}.";
+        }
+
+    }
+}
---
+++ SignalBot_NetCore/Services/TelegramService.cs
@@ -0,0 +1,76 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace SignalBot_NetCore.Services
+{
+    public class TelegramService
+    {
+        TelegramBotClient _botClient;
+        HashSet<long> _chats = new HashSet<long>();
+
+        public TelegramService()
+        {
+            _botClient = new TelegramBotClient("5788071669:AAF0a0JmiMknWZ9VqkThWcNKKq00_u_JPCI");
+            using var cts = new CancellationTokenSource();
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            };
+            _botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+        }
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // Only process Message updates: https://core.telegram.org/bots/api#message
+            if (update.Message is not { } message)
+                return;
+            // Only process text messages
+            if (message.Text is not { } messageText)
+                return;
+
+            var chatId = message.Chat.Id;
+            _chats.Add(chatId);
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+            // Echo received message text
+            Message sentMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "You said:\n" + messageText,
+                cancellationToken: cancellationToken);
+        }
+
+        Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+        public async Task SendMessageAsync(string msg)
+        {
+            foreach (var chatId in _chats)
+            {
+                Message sentMessage = await _botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: $"signal: {msg}");
+            }
+        }
+
+    }
+}
